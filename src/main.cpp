5bd570e64b7dd783f09e20f7851b0024fc2a9e8a
votre_login_github <votre_mail_github>
2026-02-20 10:10:21 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float puissances[5] = {50.0, 22.0, 150.0, 7.4};   // en kw
+    float distance[5] = {12.5, 22.0, 11.0, 150.0, 7.4};   // en kms
+
+    return 0;
+}
