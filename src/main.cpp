58e202f216fef3906b6dcd8e51e5d52b965f84f4
votre_login_github <votre_mail_github>
2026-02-20 10:57:19 +0000

    premier commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
+
 void afficherBornes(const float puiss[], const float dist[], int taille);
+
 int main() {
   float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4}; // en kW
-  float distance[5] = {12.5, 35.0, 8.0, 60.0, 2.3};   // en km
+  float distance[5] = {12.5, 35.0, 8.0, 60.0, 2.3};     // en km
 
-  for (int i = 0; i < 5; i++) {
-    cout << "Borne " << i + 1 << " : " << puissances[i] << " kW, a "
-         << distance[i] << " km" << endl;
-  }
+  afficherBornes(puissances, distance, 5);
 
   return 0;
 }
 
-void afficherBornes(const float puiss[], const float dist[], int taille){
-    for (int i = 0; i < 5; i++) {
-        cout << "Borne " << i + 1 << " : " << puiss[i] << " kW, a "
-             << dist[i] << " km" << endl;
-    }
+void afficherBornes(const float puiss[], const float dist[], int taille) {
+  for (int i = 0; i < taille; i++) {
+    cout << "Borne " << i + 1 << " : " << puiss[i] << " kW, a " << dist[i]
+         << " km" << endl;
+  }
 }
